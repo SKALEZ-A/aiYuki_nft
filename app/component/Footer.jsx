c35ee4fdc6ea448c9668f8faf2b61871a515ca6a
SKALEZ-A <holumacbeth@gmail.com>
2023-10-20 06:25:44 +0100

    new changes

--- app/component/Footer.jsx
+++ app/component/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Image from "next/image";
+import footerLogo from "../../public/Images/bottom.png";
 
 const Footer = () => {
   return (
     <footer className="border footer z-10 border-t-[#33353F] bottom-0 left-0 right-0">
       <div>
         <Image
-          src="/images/bottom.png"
+          src={footerLogo}
           className="w-full bottom-0"
           width={320}
           height={320}
